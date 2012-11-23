15ff4367ca7ed7420f29a0cf8a00a25cbd0b0d24
megadoro <6.626068h@gmail.com>
2012-11-23 18:02:45 +0100

    update

--- hard_kernel.c
+++ hard_kernel.c
@@ -18,17 +18,8 @@
 #define _vec3_dot(a,b) \
     ((a).x*(b).x+(a).y*(b).y+(a).z*(b).z)
 
-#define _vec3_mod(a) \
-    sqrt(pow((a).x,2)+pow((a).y,2)+pow((a).z,2))
-
 #define _rand() \
     (rand()/(float)RAND_MAX)
-
-double min(double a, double b)
-{
-    if(a > b) return b;
-    else return a;
-}
 
 typedef struct vec3{
     double x,y,z;
@@ -37,6 +28,20 @@ typedef struct vec3{
 vec3 VEC3(double X, double Y, double Z){
     vec3 tmp = {.x = X, .y = Y, .z = Z};
     return tmp;
+}
+
+double vec3_mod(vec3 a){
+    return sqrt( a.x * a.x + a.y * a.y + a.z * a.z);
+}
+
+double vec3_dot(vec3 a, vec3 b){
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+double min(double a, double b)
+{
+    if(a > b) return b;
+    else return a;
 }
 
 typedef struct _body{
@@ -51,6 +56,7 @@ double ETA;
 double SIGMA;
 double runtime;
 double pressure;
+double min_time;
 
 double get_min()
 {
@@ -73,7 +79,7 @@ void collide()
     double dx, dy, dz, rr, rv, vv, delta, collision_time;
     for(i = 0; i < N; i++){
         for(j = i+1; j < N; j++){
-            if(i == collider1 || i == collider2 || j == collider1 || j == collider2 || collider1 == collider2){
+            if(i == collider1 || j == collider2 || collider1 == collider2){
                 collision_time = DBL_MAX;
                 for(dx = -1; dx <= 1 ; dx++){
                     for(dy = -1; dy <= 1; dy++){
@@ -83,9 +89,9 @@ void collide()
                             dr.x += dx;
                             dr.y += dy;
                             dr.z += dz;
-                            rr = _vec3_dot(dr, dr);
-                            rv = _vec3_dot(dr, dv);
-                            vv = _vec3_dot(dv, dv);
+                            rr = vec3_dot(dr, dr);
+                            rv = vec3_dot(dr, dv);
+                            vv = vec3_dot(dv, dv);
                             delta = rv*rv-vv*(rr-SIGMA*SIGMA);
 
                             if(rv <= 0 && delta >= 0){
@@ -97,6 +103,7 @@ void collide()
                 }
                 ctimes[i*N+j] = collision_time;
             }
+            else ctimes[i*N+j] -= min_time;
         }
     }
 }
@@ -113,9 +120,6 @@ void init()
     pressure = 0;
     runtime = 0;
 
-    collider1 = 0;
-    collider2 = 0;
-
     int k;
     vec3 tmp = {.x = 0, .y = 0, .z = 0};
     for(k = 1; k < N; k++){
@@ -126,12 +130,15 @@ void init()
     particle[0].r = VEC3(_rand(), _rand(), _rand());
     particle[0].v = VEC3(-tmp.x, -tmp.y, -tmp.z);
 
+    collider1 = 0;
+    collider2 = 0;
+
     collide();
 }
 
 void run()
 {
-    double min_time = get_min();
+    min_time = get_min();
 
     int k;
     for(k = 0; k < N; k++){
@@ -149,25 +156,43 @@ void run()
     }
 
     vec3 dr, dv;
-    _vec3_sub(dr, particle[collider2].r, particle[collider1].r);
-    _vec3_sub(dv, particle[collider1].v, particle[collider2].v);
+    dr.x = particle[collider2].r.x - particle[collider1].r.x;
+    dr.y = particle[collider2].r.y - particle[collider1].r.y;
+    dr.z = particle[collider2].r.z - particle[collider1].r.z;
 
-    particle[collider1].r.x -= _vec3_dot(dv,dr)*dr.x;
-    particle[collider1].r.y -= _vec3_dot(dv,dr)*dr.y;
-    particle[collider1].r.z -= _vec3_dot(dv,dr)*dr.z;
+    dv.x = particle[collider1].v.x - particle[collider2].v.x;
+    dv.y = particle[collider1].v.y - particle[collider2].v.y;
+    dv.z = particle[collider1].v.z - particle[collider2].v.z;
 
-    particle[collider2].r.x += _vec3_dot(dv,dr)*dr.x;
-    particle[collider2].r.y += _vec3_dot(dv,dr)*dr.y;
-    particle[collider2].r.z += _vec3_dot(dv,dr)*dr.z;
+    particle[collider1].r.x -= vec3_dot(dv,dr)*dr.x;
+    particle[collider1].r.y -= vec3_dot(dv,dr)*dr.y;
+    particle[collider1].r.z -= vec3_dot(dv,dr)*dr.z;
+
+    particle[collider2].r.x += vec3_dot(dv,dr)*dr.x;
+    particle[collider2].r.y += vec3_dot(dv,dr)*dr.y;
+    particle[collider2].r.z += vec3_dot(dv,dr)*dr.z;
 
     collide();
 
-    pressure += SIGMA*_vec3_mod(dv)/2;
+    pressure += SIGMA*vec3_mod(dv)/2;
     runtime += min_time;
 }
 
 int main()
 {
+/*
+    vec3 r,v1 = VEC3(3,2,1), v2 = VEC3(1,2,3);
+    printf("\n%e\n",vec3_mod(v1));
+    printf("\n%e\n",vec3_dot(v1,v2));
+    _vec3_add(r,v1,v2);
+    printf("\n%e\n",r.x);
+    printf("\n%e\n",r.y);
+    printf("\n%e\n",r.z);
+    _vec3_sub(r,v1,v2);
+    printf("\n%e\n",r.x);
+    printf("\n%e\n",r.y);
+    printf("\n%e\n",r.z);
+*/
     int k;
     FILE *f = fopen("pressure.dat","w");
     for(ETA = 0.01; ETA < 0.5; ETA += 0.01){
