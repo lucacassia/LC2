976acd1a3a0e3d58b58890bdba5d2648daa2c2c4
megadoro <6.626068h@gmail.com>
2013-02-02 14:13:36 +0100

    update

--- ising/ising.h
+++ ising/ising.h
@@ -30,9 +30,9 @@ unsigned int height = 500;
 spin *ising = NULL;
 unsigned int step;
 
-double J = 0.5;
-double beta = 1e-8;
-double Z,mM,mE;
+long double J = 1;
+long double beta = 0.5;
+long double Z,mM,mE;
 
 void clear()
 {
@@ -53,94 +53,88 @@ void init()
     step = Z = mM = mE = 0;
 }
 
-double hamiltonian(int i, int j)
-{
-    return -J * ( ising[UP].s + ising[DOWN].s + ising[LEFT].s + ising[RIGHT].s );
-}
-
 void metropolis()
 {
     int i,j,new_spin;
-    double deltaE;
+    long double deltaE;
     for(i = 0; i < height; i++) for(j = 0; j < width; j++) {
-        deltaE = -J * ( ising[UP].s + ising[DOWN].s + ising[LEFT].s + ising[RIGHT].s );
         if( mersenne() < 0.5 ) new_spin = 1; else new_spin = -1;
-        deltaE *= new_spin - ising[CENTER].s;
-        if( mersenne() < exp(-deltaE) ) ising[CENTER].s = new_spin;
+        deltaE = -J * ( ising[UP].s + ising[DOWN].s + ising[LEFT].s + ising[RIGHT].s ) * (new_spin - ising[CENTER].s);
+        if( deltaE < 0 || mersenne() < exp(-beta*deltaE) ) ising[CENTER].s = new_spin;
     }
 }
 
-void flip_cluster(spin *s, int flip)
+void flip_cluster(spin *local, int flip)
 {
-    s->done = 1;
-    if(flip) s->s = !s->s;
+    local->done = 1;
+    if(flip) local->s = !local->s;
 
-    if(s->u != NULL){
-        s->u->d = NULL;
-        flip_cluster(s->u,flip);
+    if(local->u != NULL){
+        local->u->d = NULL;
+        flip_cluster(local->u,flip);
     }
-    if(s->d != NULL){
-        s->d->u = NULL;
-        flip_cluster(s->d,flip);
+    if(local->d != NULL){
+        local->d->u = NULL;
+        flip_cluster(local->d,flip);
     }
-    if(s->l != NULL){
-        s->l->r = NULL;
-        flip_cluster(s->l,flip);
+    if(local->l != NULL){
+        local->l->r = NULL;
+        flip_cluster(local->l,flip);
     }
-    if(s->r != NULL){
-        s->r->l = NULL;
-        flip_cluster(s->r,flip);
+    if(local->r != NULL){
+        local->r->l = NULL;
+        flip_cluster(local->r,flip);
     }
 }
 
 void SW()
 {
     int i,j,k;
-    for(k = 0; k < width*height; k++)
+    for(k = 0; k < width*height; k++){
         ising[k].done = 0;
+        ising[k].r = ising[k].l = ising[k].u = ising[k].d = NULL;
+    }
 
     for(i = 0; i < height; i++) for(j = 0; j < width; j++) {
         ising[CENTER].done = 1;
 
         if( !ising[RIGHT].done ) {
-            if( ising[RIGHT].s == ising[CENTER].s && mersenne() < 1-exp(beta*J) ){
+            if( ising[RIGHT].s == ising[CENTER].s ){
                 ising[CENTER].r = &ising[RIGHT];
                 ising[RIGHT].l = &ising[CENTER];
             }
-            else ising[RIGHT].l = ising[CENTER].r = NULL;
         }
 
         if( !ising[LEFT].done ) {
-            if( ising[LEFT].s == ising[CENTER].s && mersenne() < 1-exp(beta*J) ){
+            if( ising[LEFT].s == ising[CENTER].s ){
                 ising[CENTER].l = &ising[LEFT];
                 ising[LEFT].r = &ising[CENTER];
             }
-            else ising[LEFT].r = ising[CENTER].l = NULL;
         }
 
         if( !ising[DOWN].done ) {
-            if( ising[DOWN].s == ising[CENTER].s && mersenne() < 1-exp(beta*J) ){
+            if( ising[DOWN].s == ising[CENTER].s ){
                 ising[CENTER].d = &ising[DOWN];
                 ising[DOWN].u = &ising[CENTER];
             }
-            else ising[DOWN].u = ising[CENTER].d = NULL;
         }
 
         if( !ising[UP].done ) {
-            if( ising[UP].s == ising[CENTER].s && mersenne() < 1-exp(beta*J) ){
+            if( ising[UP].s == ising[CENTER].s ){
                 ising[CENTER].u = &ising[UP];
                 ising[UP].d = &ising[CENTER];
             }
-            else ising[UP].d = ising[CENTER].u = NULL;
         }
     }
 
-    for(k = 0; k < width*height; k++)
+/*    for(k = 0; k < width*height; k++)
         ising[k].done = 0;
 
     for(k = 0; k < width*height; k++) if(!ising[k].done)
-        flip_cluster(&ising[k], (mersenne() < 0.5) );
+        flip_cluster(&ising[k], 1 );
+*/
 
+    flip_cluster(&ising[width*height/2],1);
 }
 
 void run()
@@ -149,16 +143,19 @@ void run()
     else     SW();
 
     if(step%10 == 0){
-        double E, M;
-        int k;
-        for(M = E = k = 0; k < width*height; k++){
-            E += hamiltonian(k/width, k%width);
-            M += ising[k].s;
+        long double E;
+        long long int M;
+        int i,j;
+        E = M = 0;
+        for(i = 0; i < height; i++) for(j = 0; j < width; j++) {
+            E += -J * ( ising[UP].s + ising[DOWN].s + ising[LEFT].s + ising[RIGHT].s ) * ising[CENTER].s;
+            M += ising[CENTER].s;
         }
-        Z += exp(-beta*E);
-        mM += M*exp(-beta*E);
-        mE += E*exp(-beta*E);
-//        printf("\n<e> = %.10e\t<M> = %.10e\n",mE/(Z*width*height),mM/(Z*width*height));
+        printf("M = %Ld\tB = %Le\n",M,E);
+        Z += expl(-beta*E);
+        mM += M*expl(-beta*E);
+        mE += E*expl(-beta*E);
+        //printf("\n<e> = %.10Le\t<M> = %.10Le\tZ = %.10Le\n",mE/(step*width*height),mM/(step*width*height),Z);
     }
     step++;
 }
