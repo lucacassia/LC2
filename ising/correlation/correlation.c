a4357c0805a6305c44a5c295b2ef757d5f5bb3fe
megadoro <6.626068h@gmail.com>
2015-06-24 18:09:30 +0200

    update

--- ising/correlation/correlation.c
+++ ising/correlation/correlation.c
@@ -1,27 +1,58 @@
 #include"ising.h"
 
-void plot_correlation(void (*algorithm)(int), int bin_number, int n){
-
-    int t;
-    double beta_value;
+void get_correlation(void (*algorithm)(), int bin_number, double beta_value){
+    int i, j, t, dist, bin_size = get_bin_size(algorithm);
     char filename[50];
-    if(algorithm == MH){ sprintf(filename,"data/correlation_%d_MH.dat",bin_number); }
-    if(algorithm == SW){ sprintf(filename,"data/correlation_%d_SW.dat",bin_number); }
-    FILE *f = fopen(filename,"w");
+    sprintf(filename, "data/%s_correlation_%f_%d.dat", get_algorithm_string(algorithm), beta_value, bin_number);
 
-    for(beta_value = 0.0f; beta_value <= 1.0f; beta_value += 1.0f / n ){
-        double *binned_data = get_binned_data(algorithm, beta_value, bin_number, func);
-        for(t = 0; t < bin_number; t++){
-            fprintf(f,"%f\n",binned_data[t]);
+    init(beta_value);
+    printf("\nExecuting %s @ β = %f\n\n", get_algorithm_string(algorithm), beta_value);
+    printf("Thermalizing................"); fflush(stdout); thermalize(algorithm); printf("DONE!\n");
+
+    double *Sx, *Sy;
+    double **correlation = (double**)malloc((size/2)*sizeof(double*));
+    for(dist = 0; dist < size / 2; dist++) correlation[dist] = (double*)malloc(bin_number * sizeof(double));
+
+    printf("Gathering Binned Data......."); fflush(stdout);
+    for(t = 0; t < bin_number * bin_size; t++){
+        algorithm();
+        Sx = (double*)malloc(size * sizeof(double));
+        Sy = (double*)malloc(size * sizeof(double));
+        for(i = 0; i < size; i++){ Sx[i] = Sy[i] = 0.0f; }
+        for(i = 0; i < size; i++) for(j = 0; j < size; j++){
+            Sx[j] += ising[i][j].s / size;
+            Sy[i] += ising[i][j].s / size;
         }
-    }
-    fclose(f);
-    free(binned_data);
+        for(dist = 0; dist < size / 2; dist++) for(i = 0; i < size; i++){
+            correlation[dist][t / bin_size] += Sx[i] * Sx[(i+dist)%size];
+            correlation[dist][t / bin_size] += Sx[i] * Sx[(size+i-dist)%size];
+            correlation[dist][t / bin_size] += Sy[i] * Sy[(i+dist)%size];
+            correlation[dist][t / bin_size] += Sy[i] * Sy[(size+i-dist)%size];
+            correlation[dist][t / bin_size] /= 4 * size * bin_size;
+        }
+        free(Sx);
+        free(Sy);
+    }printf("DONE!\t%d Samples x %d Bin Gathered\n\n", bin_size, bin_number);
 
-    printf("Written to: %s\n",filename);
+    FILE *f = fopen(filename,"w");
+    double mean, variance;
+    for(dist = 0; dist < size / 2; dist++){
+        variance = mean = 0.0f;
+        for(t = 0; t < bin_number; t++){
+            mean += correlation[dist][t];
+            variance += correlation[dist][t] * correlation[dist][t];
+        }
+        free(correlation[dist]);
+        mean /= bin_number;
+        variance = variance / bin_number - mean * mean;
+        fprintf(f,"%d\t%f\t%f\t\n", dist, mean, variance);
+    }
+    free(correlation);
+    fclose(f);
 }
 
 int main(){
-
+    size = 128;
+    get_correlation(MH,50,0.3);
     return 0;
 }
--- ising/correlation/correlation.gnu
+++ ising/correlation/correlation.gnu
@@ -1,23 +1,3 @@
-## energy binning
-
-#set title "Binning"
-set xlabel "Markov time"
-set ylabel "energy density"
-set terminal postscript eps size 4.5,4.5 enhanced color dashed lw 1 "DejaVuSans" 12
-
-set output 'img/energy_bin_100_MH.eps'
-plot 'data/energy_bin_100_MH.dat'   pt 1 ps 0.5 lc rgb '#000000' title 'MH  N_B = 100 \@ {/Symbol b} = 0.3'
-
-set output 'img/energy_bin_100_SW.eps'
-plot 'data/energy_bin_100_SW.dat'   pt 1 ps 0.5 lc rgb '#000000' title 'SW  N_B = 100 \@ {/Symbol b} = 0.3'
-
-set output 'img/energy_bin_1000_MH.eps'
-plot 'data/energy_bin_1000_MH.dat'   pt 1 ps 0.5 lc rgb '#000000' title 'MH  N_B = 1000 \@ {/Symbol b} = 0.3'
-
-set output 'img/energy_bin_1000_SW.eps'
-plot 'data/energy_bin_1000_SW.dat'   pt 1 ps 0.5 lc rgb '#000000' title 'SW  N_B = 1000 \@ {/Symbol b} = 0.3'
-
-
 set xlabel "{/Symbol b}"
 set ylabel "energy density"
 set yrange[-2:0]
