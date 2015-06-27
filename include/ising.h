1c534f6e9567048171bedba3101ec4af0196b57b
megadoro <6.626068h@gmail.com>
2015-06-27 13:51:01 +0200

    update

--- include/ising.h
+++ include/ising.h
@@ -157,25 +157,102 @@ void dump_data(int lattice_size, double beta_value, void (*algorithm)(), int run
 {
     if( algorithm != MH && algorithm != SW ){ printf("\nInvalid Algorithm!\n"); }
     else{
-        char filename[50];
-        sprintf(filename, "data/%d_%f_%s_%d.dat", lattice_size, beta_value, get_algorithm_string(algorithm), run_time);
-        FILE *f = fopen(filename, "w");
-        fprintf(f, "#\t%d\t%f\t%s\n", lattice_size, beta_value, get_algorithm_string(algorithm));
+        int id = (algorithm == SW);
+        /* .dat header */
+        char filename_dat[50];
+        sprintf(filename_dat, "data/%d_%f_%s_%d.dat", lattice_size, beta_value, get_algorithm_string(algorithm), run_time);
+        FILE *f_dat = fopen(filename_dat, "w");
+        fprintf(f_dat, "#\t%d\t%f\t%s\n", lattice_size, beta_value, get_algorithm_string(algorithm));
+        /* .bin header */
+        char filename_bin[50];
+        sprintf(filename_bin, "data/%d_%f_%s_%d.bin", lattice_size, beta_value, get_algorithm_string(algorithm), run_time);
+        FILE *f_bin = fopen(filename_bin, "wb");
+        int cols = 4;
+        fwrite(&cols, sizeof(int), 1, f_bin);
+        fwrite(&lattice_size, sizeof(int), 1, f_bin);
+        fwrite(&beta_value, sizeof(double), 1, f_bin);
+        fwrite(&id, sizeof(int), 1, f_bin);
+        fwrite(&run_time, sizeof(int), 1, f_bin);
+        /* data gathering */
         printf("\nExecuting %s : L = %d : β = %f : time = %d\n\n", get_algorithm_string(algorithm), lattice_size, beta_value, run_time);
         init(lattice_size, beta_value);
         printf("Gathering Data..."); fflush(stdout);
-        int t; for(t = 0; t < run_time; t++){
-            fprintf(f, "%f", get_energy() / (lattice_size * lattice_size));
-            fprintf(f, "\t%f", get_magnetization() / (lattice_size * lattice_size));
-            if(algorithm == MH) fprintf(f, "\t%f", get_magnetization() / (lattice_size * lattice_size));
-            if(algorithm == SW) fprintf(f, "\t%f", (get_largest_cluster() * 1.0f) / (lattice_size * lattice_size));
-            fprintf(f, "\n");
+        double tmp; int t;
+        for(t = 0; t < run_time; t++){
+            tmp = get_energy() / (lattice_size * lattice_size);
+            fprintf(f_dat, "%f", tmp);
+            fwrite(&tmp, sizeof(double), 1, f_bin);
+
+            tmp = get_magnetization() / (lattice_size * lattice_size);
+            fprintf(f_dat, "\t%f", tmp);
+            fwrite(&tmp, sizeof(double), 1, f_bin);
+
+            if(algorithm == MH){
+                fprintf(f_dat, "\t%f", tmp);
+                fwrite(&tmp, sizeof(double), 1, f_bin);
+            }
+            if(algorithm == SW){
+                tmp = (get_largest_cluster() * 1.0f) / (lattice_size * lattice_size);
+                fprintf(f_dat, "\t%f", tmp);
+                fwrite(&tmp, sizeof(double), 1, f_bin);
+            }
+
+            fwrite(&tmp, sizeof(double), 1, f_bin); /* correlation (todo) */
+
+            fprintf(f_dat, "\n");
             algorithm();
         }
         printf(" DONE!\n\n");
-        fclose(f);
+        fclose(f_dat);
+        fclose(f_bin);
         clear();
     }
+}
+
+typedef struct _raw{
+    int l;              /* lattice size */
+    double b;           /* beta */
+    char *algorithm;    /* algorithm name */
+    int size;           /* number of samples */
+    double *data;       /* samples */
+}raw;
+
+void raw_close(raw *obj)
+{
+    if(obj->data != NULL) free(obj->data);
+    obj->l = 0;
+    obj->b = 0.0f;
+    obj->algorithm = NULL;
+    obj->size = 0;
+    obj->data = NULL;
+}
+
+raw load_data(FILE *f, int column)
+{
+    raw content = { 0, 0.0f, NULL, 0, NULL };
+    int cols; fread(&cols, sizeof(int), 1, f );
+    if(column < 0 || column > cols) return content;
+    /* read lattice size */
+    fread(&content.l,sizeof(int),1,f);
+    /* read beta value */
+    fread(&content.b,sizeof(double),1,f);
+    /* read algorithm name */
+    int id;
+    fread(&id,sizeof(int),1,f);
+    if(id) content.algorithm = "MH";
+    else   content.algorithm = "SW";
+    /* read size */
+    fread(&content.size,sizeof(int),1,f);
+    /* read data */
+    content.data = (double*)malloc(content.size * sizeof(double));
+    int t;
+    for(t = 0; t < content.size; t++){
+        fseek(f,column*sizeof(double),SEEK_CUR);
+        fread(&content.data[t],sizeof(double),1,f);
+        fseek(f,((cols-1)-column)*sizeof(double),SEEK_CUR);
+    }
+    fseek(f, 0L, SEEK_SET);
+    return content;
 }
 
 /*
