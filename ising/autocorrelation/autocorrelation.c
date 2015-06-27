85e717971363cee03905ce3a0c55558537b282c8
megadoro <6.626068h@gmail.com>
2015-06-27 15:12:23 +0200

    update

--- ising/autocorrelation/autocorrelation.c
+++ ising/autocorrelation/autocorrelation.c
@@ -48,10 +48,15 @@ void get_autocorrelation(char *input){
 int main(){
 
     char filename[50];
-    sprintf(filename, "data/32_0.430000_MH_100000.bin");
-    printf("\nReading from: %s\n", filename);
-
-    get_autocorrelation(filename);
+    double x;
+    for(x = -0.1; x <= 0.1; x += 0.004){
+        sprintf(filename, "data/32_%f_MH_100000.bin",(x+1) * get_beta_critical());
+        printf("\nReading from: %s\n", filename);
+        get_autocorrelation(filename);
+        sprintf(filename, "data/32_%f_SW_100000.bin",(x+1) * get_beta_critical());
+        printf("\nReading from: %s\n", filename);
+        get_autocorrelation(filename);
+    }
 
     return 0;
 }
