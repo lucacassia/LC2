9c470e2479fbb222a21d76146e2eb05dad188506
megadoro <6.626068h@gmail.com>
2015-07-17 21:11:21 +0200

    update

--- hard_core_2d/hardcore2d.c
+++ hard_core_2d/hardcore2d.c
@@ -1,62 +1,60 @@
 /*
- * USAGE: ./hard_core_2d    <eta> 
+ * USAGE: ./hard_core_2d    <ETA> 
  *
- *   default eta = 0.1f
+ *   default ETA = 0.1f
  */
 
+#define K_BOLTZ 1.0f
 
-#include "hard_core_2d.h"
+#include "hardcore2d.h"
 
 int main (int argc, char *argv[])
 {
     /* INIT VARI */
     /*Calcola il numero di istanti temporali che verranno salvati*/
-    NUM_TEMPI_SALVATI = (int)(floor( (double) TIME_MAX / DeltaT)+1);
+    n_history = (int)(floor(TIME_MAX*1.0f/time_step)+1);
     unsigned int i;
     init();
     double dist_tot = 0.0f;
-    double fraz_imp = 0.1f;
-    if(argc > 1) fraz_imp = atof(argv[1]);
+    double ETA = 0.1f;
+    if(argc > 1) ETA = atof(argv[1]);
 
-    SIGMA = sqrt(4*fraz_imp/ n_particles / M_PI);
-    /* DA dove salta fuori?*/
-    DIST_RET = sqrt(4*0.74/ n_particles / M_PI);
+    SIGMA = 3.54490770181 * sqrt( ETA / (double)n_particles );
+
+/*    DIST_RET = sqrt(4*0.74/ n_particles / M_PI);*/
+
+
     printf("\n\n*****************************************************\n");
     printf("Starting simulation with:\n");
     printf("SIGMA = %e\t",SIGMA);
-    printf("Frazione di impacchettamento: %e\n", fraz_imp);
+    printf("Frazione di impacchettamento: %e\n", ETA);
 
     init();
     fix_boundaries();
 
-    if ( check_distance() != 0 ){
-        printf("Sfere troppo vicine tra loro. Avvio annullato\n");
-        exit(EXIT_FAILURE);
-    }
-
     temperature = 2*get_kinetic()/((double) DIMENSION)/(double) n_particles/K_BOLTZ;
-    printf(" K = %e \t P= %e \t", get_kinetic(), total_momentum());
+    printf(" K = %e \t P= %e \t", get_kinetic(), get_total_momentum());
     printf("Temperature is: %f \n",temperature );
-    riscala_vel_temp();
+    set_temperature(1.0f);
     temperature = 2*get_kinetic()/((double) DIMENSION)/(double) n_particles/K_BOLTZ;
-    printf(" K = %e \t P= %e \t", get_kinetic(), total_momentum());
+    printf(" K = %e \t P= %e \t", get_kinetic(), get_total_momentum());
     printf("Temperature is: %f \n",temperature );
 
     /****** GESTIONE FILE  ******/
     char r2_file[64] = "";
-    snprintf(r2_file,64,"data/dr2/dr2_%d_%.6lf.dat",(int)time(NULL),fraz_imp); 
+    snprintf(r2_file,64,"data/dr2/dr2_%d_%.6lf.dat",(int)time(NULL),ETA); 
     /*char * press_file = "data/press.dat";*/
     char  tc_filename[64] = "";
-    snprintf(tc_filename, 64, "data/tc/%d/tc%6f.dat",n_particles, fraz_imp);
+    snprintf(tc_filename, 64, "data/tc/%d/tc%6f.dat",n_particles, ETA);
 
     char tcpdf_filename[64] = "";
-    snprintf(tcpdf_filename, 64, "data/pdf_tc/%d/%6f.dat",n_particles, fraz_imp);
+    snprintf(tcpdf_filename, 64, "data/pdf_tc/%d/%6f.dat",n_particles, ETA);
     char mfp_filename[64] = "";
-    snprintf(mfp_filename,64,"data/mfp/%d/mfp%.6lf.dat",n_particles,fraz_imp);
+    snprintf(mfp_filename,64,"data/mfp/%d/mfp%.6lf.dat",n_particles,ETA);
 
     /****FINE GESTIONE FILE***/
-    char  pression_filename[128] = "";
-    snprintf(pression_filename,128,"data/pression/%d/pression%.6lf.dat",n_particles,fraz_imp);
+    char  pressure_filename[128] = "";
+    snprintf(pressure_filename,128,"data/pressure/%d/pressure%.6lf.dat",n_particles,ETA);
 
 
     print_pos();
@@ -65,44 +63,44 @@ int main (int argc, char *argv[])
     FILE *pdf_tc_file = fopen(tcpdf_filename,"w");
 
     /* Evolution */
-    collision_table();
+    get_collision_table();
     while( numOfCollisions < THERMALIZATION_TIME ){ evolve_therm(); }
 
     numOfCollisions = 0;
     reset_mfp_variables();
     print_distribution();
     total_time = 0;
-    pression=0;
+    pressure=0;
     printf("Termalizzato: %d urti ---- kin_en = %lf\n",numOfCollisions,get_kinetic());
     while(total_time < TIME_MAX){
         evolve();
-        fprintf(pdf_tc_file,"%lf\n",time_collision*n_particles/2.0);
+        fprintf(pdf_tc_file,"%lf\n",min_time*n_particles/2.0);
     }
     printf("Num collisioni: %d\n",numOfCollisions);
     fclose(pdf_tc_file);
-    if(time_counted > NUM_TEMPI_SALVATI){
+    if(time_counted > n_history){
         printf("ERROR \n");
     }
     r_squared_save(r2_file);
 
     /****** CALCOLO PV/NKT = 1 + 1/(3*n_particles*k_boltz*temp)*massa*diametro*Somma collisioni******/
-    pression /= (double) (3*(total_time)*get_kinetic());
-    pression *= SIGMA;
-    pression +=1.0;
-    pression *= (fraz_imp/0.9069); /*dovuto a PV_0/NKT*/
+    pressure /= (double) (3*(total_time)*get_kinetic());
+    pressure *= SIGMA;
+    pressure +=1.0;
+    pressure *= (ETA/0.9069); /*dovuto a PV_0/NKT*/
     FILE *f_collision=fopen(tc_filename,"a");
-    fprintf(f_collision,"%e\t%e\n",fraz_imp,total_time/(2*numOfCollisions)*(n_particles));
+    fprintf(f_collision,"%e\t%e\n",ETA,total_time/(2*numOfCollisions)*(n_particles));
 
-    FILE * file_pression = fopen(pression_filename,"a");
-    fprintf(file_pression,"%e\n",pression);
-    fclose(file_pression);
+    FILE * file_pressure = fopen(pressure_filename,"a");
+    fprintf(file_pressure,"%e\n",pressure);
+    fclose(file_pressure);
 
     FILE *f_mean_mfp = fopen( mfp_filename,"a");
     for(i = 0; i < n_particles; i++)
-        dist_tot += particle[i].distance/(double)(particle[i].n_collision); 
+        dist_tot += particle[i].distance/(double)(particle[i].n_collisions); 
 
     dist_tot /= (double)(n_particles);
-    fprintf(f_mean_mfp, "%.14e\t%.14e\t\n", fraz_imp, dist_tot);
+    fprintf(f_mean_mfp, "%.14e\t%.14e\t\n", ETA, dist_tot);
     fclose(f_mean_mfp);
     fclose(f_collision);
     clean();
