611e96cc357a14611d360ff5cb99adf78b67c346
megadoro <6.626068h@gmail.com>
2015-07-27 14:15:34 +0200

    update

---
+++ softcore/pressure/pressure.c
@@ -0,0 +1,68 @@
+/*
+ *  usage: ./pressure > output
+ */
+
+#define DIMENSION 3
+
+#include"softcore.h"
+
+int main(int argc, char *argv[])
+{
+    /* MD settings */
+    N = 108;
+    rho = 0.6;
+    L = pow(N/rho, 1.0f/DIMENSION);
+    double TEMP = 1.22;
+
+    int total_steps = 11e4;
+
+    /* allocate memory */
+    obj *particle = (obj*)malloc(N*sizeof(obj));
+    int **neighbour = NULL;
+    neighbour = create_table(neighbour);
+
+    init_pos(particle,N/2,0.0);
+    init_pos(&particle[N/2],N-N/2,0.5);
+
+    init_mom(particle);
+    reset_mom(particle,TEMP/T);
+    compute_table(particle,neighbour);
+    get_acc(particle,neighbour);
+
+    /* print header */
+    printf("#N=%d rho=%f L=%f T=%f dt=%f\n", N, rho, L, T, dt);
+    printf("#\tt\tH\tU\tK\tT\n");
+
+    double sumU = 0.0f;
+    double sumU2 = 0.0f;
+    double sumT = 0.0f;
+    double sumT2 = 0.0f;
+    double sumP = 0.0f;
+    double sumP2 = 0.0f;
+
+    /* simulation run */
+    int i, count = 0;
+    for(i = 0; i < total_steps; i++){
+        if(!(i%10)) compute_table(particle,neighbour);
+        integrate(particle,neighbour);
+        if((i*dt<10)&&(i%10==0)) reset_mom(particle,TEMP/T);
+        if((i*dt>10)&&(i%100==0)){
+            sumU += U;
+            sumU2 += U*U;
+            sumT += T;
+            sumT2 += T*T;
+            sumP += P/(DIMENSION*N*T*i)-1;
+            sumP2 += (P/(DIMENSION*N*T*i)-1)*(P/(DIMENSION*N*T*i)-1);
+            count++;
+        }
+        printf("%e %e %e %e %e %e\n", i*dt, H/N, U/N, K/N, T, P/(DIMENSION*rho*T*i) );
+    }
+    printf("# <U/N>=%e\tsqrt[<(U/N-<U/N>)²>]=%e\n", (sumU/count)/N, sqrt(sumU2/count-(sumU/count)*(sumU/count))/N );
+    printf("# <T>=%e\tsqrt[<(T-<T>)²>]=%e\n", (sumT/count), sqrt(sumT2/count-(sumT/count)*(sumT/count)) );
+    printf("# <P>=%e\tsqrt[<(P-<P>)²>]=%e\n", (sumP/count), sqrt(sumP2/count-(sumP/count)*(sumP/count)) );
+
+    /* exit */
+    free(particle);
+    destroy_table(neighbour);
+    return 0;
+}
---
+++ softcore/pressure/pressure.gnu
@@ -0,0 +1,58 @@
+#!/usr/bin/gnuplot
+
+reset
+
+set terminal postscript eps size 6,4.5 enhanced color dashed lw 1 "DejaVuSans" 20
+
+
+##internal energy
+set title 'Potential Energy'
+
+set ylabel 'U/N'
+set xlabel 'time'
+
+set xrange[10:110]
+set output 'img/obsU.eps'
+plot "data/output" u 1:3 w l lc 1 lw 2 t 'N = 108'
+
+
+##temperature
+set title 'Temperature'
+
+set ylabel 'kT'
+set xlabel 'time'
+
+set xrange[10:110]
+set output 'img/obsT.eps'
+plot "data/output" u 1:5 w l lc 3 lw 2 t 'N = 108'
+
+
+##pressure
+set title 'Pressure'
+
+set ylabel 'P/{/Symbol r}kT-1'
+set xlabel 'time'
+
+set xrange[10:110]
+set output 'img/obsP.eps'
+plot "data/output" u 1:6 w l lc 3 lw 2 t 'N = 108'
+
+
+##all
+set title 'N = 108 {/Symbol r} = 0.6'
+set xlabel 'time'
+unset ylabel
+
+set yrange[-4:3]
+set xrange[10:110]
+set output 'img/obsA.eps'
+plot "data/output" u 1:4 w l lc 6 t 'K/N',\
+     "data/output" u 1:5 w l lc 3 t 'T',\
+     "data/output" u 1:2 w l lc 7 t 'H/N',\
+     "data/output" u 1:3 w l lc 1 t 'U/N'
+     
+
+
+
+
+
